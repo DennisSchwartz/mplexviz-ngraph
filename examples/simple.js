2e1a33bfd7d7fdb1ba8be37ce04cdfe4afdefe00
Dennis Schwartz <schwartz_dennis@web.de>
2015-10-02 13:45:23 +0100

    Force-directed 3D layout with coloured nodes and edges works.

--- examples/simple.js
+++ examples/simple.js
@@ -1,18 +1,55 @@
-var input = {};
 
-input.nodes = 'id,Time,name\n1,now,1\n2,now,2\n3,now,3\n4,now,4\n5,then,1\n6,then,2\n7,then,3\n8,then,4';
-input.edges = 'source, target\n1, 4\n2, 3\n2, 4\n1, 5\n2, 6\n3, 8\n4, 8\n5, 6\n5, 7\n6, 7';
-input.aspects = 'Time';
-input.options = {inputFiles: 'single'};
+
+//input.nodes = 'id,Time,name\n1,now,1\n2,now,2\n3,now,3\n4,now,4\n5,then,1\n6,then,2\n7,then,3\n8,then,4';
+//input.edges = 'source, target\n1, 4\n2, 3\n2, 4\n1, 5\n2, 6\n3, 8\n4, 8\n5, 6\n5, 7\n6, 7';
+//input.aspects = 'Time';
+
+//input.edges = 'source,l1,l2,target,l1,l2\n1,A,X,2,A,X\n1,A,X,1,B,X\n1,A,X,4,B,X\n1,B,X,1,B,Y\n1,B,X,3,B,X\n\
+//1,B,X,4,B,X\n3,B,X,4,B,X\n4,B,X,3,A,Y\n3,A,Y,3,A,X\n3,A,Y,2,A,Y';
+
+function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0];
+
+    if (f) {
+        var r = new FileReader();
+        r.onload = function(e) {
+            var contents = e.target.result;
+            startRendering(contents);
+        };
+        r.readAsText(f);
+    } else {
+        alert("Failed to load file");
+    }
+}
+
+document.getElementById('inputFile').addEventListener('change', readSingleFile, false);
+//var x = document.getElementById("inputFile");
+//var reader = new FileReader();
+//console.log(reader.readAsText(x));
+var startRendering = function (contents) {
+    var input = {};
+
+    input.options = {
+        inputFiles: 'single',
+        inputFileDelimiter: ';',
+        sourceFieldLabel: 'source_name',
+        targetFieldLabel: 'target_name'
+    };
+
+    input.edges = contents;
+    console.log(input.edges);
+
+    var app = require("mplexviz-ngraph");
+    var instance = new app({
+        el: rootDiv,
+        text: 'biojs',
+        input: input
+    });
+
+    instance.render();
+};
 
 
 //var test = new Network(edges, nodes, aspects);
 
-var app = require("mplexviz-ngraph");
-var instance = new app({
-    el: rootDiv,
-    text: 'biojs',
-    input: input
-});
-
-instance.render();
